9c8319113f969e7f026886660340bead9767ccb7
ahbakken <ahbakken@uio.no>
2022-06-22 19:43:34 +0200

    Variables

--- JS_sample.js
+++ JS_sample.js
@@ -1,15 +1,22 @@
 console.log("-------------------------")
 //console.log("test")
 
-var number = 5 //comment in line
+var firstName = "Larissa"
+var age = 27
+var b = "Eu te amo mais!"
+var isFemale = true
+var balance = 100.2980
+var dob = new Date(2000, 0, 30)
+//date is written 2000-01-29T23:00:00.000Z
 
-/*
-Comment
-on multiple
-lines
-*/
+var person = {}
+var empty = undefined
 
-b = "Eu te amo mais, Astrid!"
-console.log("Larissa sier; ", b)
+//this code prints the results
+console.log(firstName, "sier; ", b)
+console.log(balance)
+console.log(dob)
+console.log(person)
+console.log(empty)
 
 console.log("-------------------------")

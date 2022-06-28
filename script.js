65698304d5ecec1d45b5a1d7d0c9e80c97a4adca
ahbakken <ahbakken@uio.no>
2022-06-28 22:23:57 +0200

    finished the video

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log("JS is almost as cool as Dengo");
 const test = () => {
+    //do something
     return "te amo";
 }
-console.log(test());
+
+const resultTest = test();
+console.log(resultTest);
+
+const greeting = "Hello";
+document.getElementById("box2").addEventListener("click", function() {
+    alert("I got clocked");
+});

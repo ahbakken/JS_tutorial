104b0a29f0c391e6a41ec0577b697c7633757d6c
ahbakken <ahbakken@uio.no>
2022-06-28 21:29:30 +0200

    massive update JS and dengo tutorial

--- python_sample.py
+++ python_sample.py
@@ -2,5 +2,5 @@ print("Hello World")
 print("Sample writing")
 print("Hei")
 a = 2
-B = 3
+b = 3
 print("Sum: ", a+b)
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("JS is almost as cool as Dengo");
+const test = () => {
+    return "te amo";
+}
+console.log(test());
